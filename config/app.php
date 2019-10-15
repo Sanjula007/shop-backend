5803d9010c3ddd2bf893997e88368b00369b0ee6
Sanjula007 <sanjula.madumal@gmail.com>
2019-10-15 16:17:40 +0530

    add image.intervention package

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
 		Barryvdh\Cors\ServiceProvider::class,
+		Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -227,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+		'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
